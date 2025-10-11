e638e3043ee6fc6e2cd3182a1eb7ff0f617a5182
Sandeep <sandeep.p.rukmani@gmail.com>
2025-10-11 19:30:47 +0530

    rtrs

--- main.py
+++
@@ -1,6 +0,0 @@
-from app import app, socketio
-
-if __name__ == '__main__':
-    import os
-    port = int(os.environ.get('PORT', 7890))
-    socketio.run(app, port=port, debug=True, allow_unsafe_werkzeug=True)
